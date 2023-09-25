e070d7ee670ee294552bed270078668684e02269
ahmed <elshehry@gamil.com>
2023-09-25 17:38:50 +0300

    Handeled favorite list 'Add & Remove'

--- pages/api/random.ts
+++ pages/api/random.ts
@@ -10,14 +10,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     await serverAuth(req, res);
 
-    const movies = await prismadb.movie.findMany();
+    const moviesCount = await prismadb.movie.count();
+    const randomIndex = Math.floor(Math.random() * moviesCount);
 
-    console.log(movies);
-    
+    const randomMovies = await prismadb.movie.findMany({
+      take: 1,
+      skip: randomIndex
+    });
 
-    return res.status(200).json(movies);
+    return res.status(200).json(randomMovies[0]);
   } catch (error) {
-    console.log({ error })
+    console.log(error);
+
     return res.status(500).end();
   }
 }
